229a1e565ba891b0982ffdcfd2183c3a7db012f1
MinhLong <longbo1208@gmail.com>
2024-03-03 22:20:42 +0700

    Product Card

--- src/components/BestSeller.js
+++ src/components/BestSeller.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { apiGetProducts } from './../apis/product';
 import { apiGetCategories } from './../apis/app';
 import Slider from "react-slick"
@@ -27,34 +27,46 @@ const BestSeller = () => {
     const [bestSeller, setBestSeller] = useState(null)
     const [newProducts, setNewProducts] = useState(null)
     const [activeTab, setActiveTab] = useState(1)
-    const [product, setProducts] = useState(null)
+    const [products, setProducts] = useState(null)
     const fetchProducts = async () => {
         const res = await Promise.all([apiGetProducts({ sort: '-sold' }), apiGetCategories({ sort: '-createdAt' })])
-        if (res[0]?.success) setBestSeller(res[0].products)
+        if (res[0]?.success) {
+            setBestSeller(res[0].products)
+            setProducts(res[0].products)
+        }
         if (res[1]?.success) setNewProducts(res[1].products)
+        setProducts(res[0].products)
     }
     // useEffect(() => {
     //     fetchProducts()
     // }, [])
+    useEffect(() => {
+        if (activeTab === 1) {
+            setProducts(bestSeller)
+        } else {
+            setProducts(newProducts)
+        }
+    }, [activeTab])
     return (
         <div>
-            <div className='flex text-[20px] gap-8 pb-4 border-b-2 border-main'>
+            <div className='flex text-[20px] border-main ml-8'>
                 {tabs.map(el => (
                     <span
                         key={el.id}
-                        className={`${activeTab === el.id ? 'text-gray-950' : ''} font-semibold capitalize border-r text-gray-400 cursor-pointer`}
+                        className={`${activeTab === el.id ? 'text-gray-950' : ''} font-semibold capitalize border-r px-8 text-gray-400 cursor-pointer`}
                         onClick={() => setActiveTab(el.id)}
                     >
                         {el.name}
                     </span>
                 ))}
             </div>
-            <div className='mt-4 mx-[-10px]'>
+            <div className='mt-4 mx-[-10px] border-b-2 border-main pt-4'>
                 <Slider {...settings}>
-                    {bestSeller?.map(el => (
+                    {products?.map(el => (
                         <ProductItem
                             key={el.id}
                             productData={el}
+                            isNew={activeTab === 1 ? false : true}
                         />
                     ))}
                 </Slider>
--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { formatMoney } from '../utils/helper'
 import label from '../assets/label.webp'
+import labelBlue from '../assets/labelBlue.png'
 
-const ProductItem = ({ productData }) => {
+const ProductItem = ({ productData, isNew }) => {
     return (
         <div className='w-full text-base border px-[10px]'>
             <div className='w-full border p-[15px] flex-col flex items-center'>
@@ -12,8 +13,12 @@ const ProductItem = ({ productData }) => {
                         alt="Images"
                         className='w-[243px] h-[243px] object-cover'
                     />
-                    <img src={label} alt="" className='absolute top-[-15px] left-[-38px] w-[100px] h-[35px] object-cover' />
-                    <span className='font-bold absolute top-[-15px] left-[-12px] text-white'>News</span>
+                    <img
+                        src={isNew ? label : labelBlue}
+                        alt=""
+                        className={`absolute w-[120px] top-[-32px] left-[-42px] h-[35px] object-contain `}
+                    />
+                    <span className={`font-bold absolute top-[-10px] left-[-12px] text-white ${isNew ? "" : "text-sm"}`}>{isNew ? "News" : "Trending"}</span>
                 </div>
                 <div className='flex flex-col gap-2 mt-[15px] items-start gap-1 w-full'>
                     <span className='line-clamp-1'>{productData?.title}</span>
